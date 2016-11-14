b2e3570e1d5456c871bdcf7e279a0f5d73cf7e30
Pedro Magalhães <phmb@cesar.org.br>
2016-11-14 20:57:28 -0300

    Setting up GoogleMaps

--- OpenWeatherMap/OpenWeatherMap/MapViewController.swift
+++ OpenWeatherMap/OpenWeatherMap/MapViewController.swift
@@ -7,29 +7,22 @@
 //
 
 import UIKit
+import GoogleMaps
 
-class MapViewController: UIViewController {
+class MapViewController: UIViewController, GMSMapViewDelegate {
 
+    @IBOutlet weak var mapView: GMSMapView?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
+        self.mapView?.delegate = self
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    //MARK: GMSMapViewDelegate
+    
+    func mapView(_ mapView: GMSMapView, didTapAt coordinate: CLLocationCoordinate2D) {
+        
     }
-    */
 
 }
